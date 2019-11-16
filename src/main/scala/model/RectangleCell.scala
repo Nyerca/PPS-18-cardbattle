3354054a9f6d9c3c1c59dce290176f7fb1f06d94
enrico.facciani <enrico.facciani@studio.unibo.it>
2019-11-16 18:23:13 +0100

    Add cells images to rectangles of the map

--- src/main/scala/model/RectangleCell.scala
+++ src/main/scala/model/RectangleCell.scala
@@ -1,7 +1,12 @@
 package model
 
+import javafx.scene.paint.ImagePattern
+import scalafx.scene.SnapshotParameters
+import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.{Circle, Rectangle}
+import scalafx.Includes._
+
 import scala.collection.mutable.ListBuffer
 
 class RectangleCell (top: Boolean, right: Boolean, bottom: Boolean, left: Boolean, elementWidth: Double = 200, elementHeight: Double = 200, elementX:Double, elementY:Double, paint:Color) extends Rectangle {
@@ -33,6 +38,48 @@ class RectangleCell (top: Boolean, right: Boolean, bottom: Boolean, left: Boolea
   super.y_=(elementY);
   super.fill_=(paint);
 
+
+  def createImage(url: String, rotation: Double): Unit = {
+    val iv = new ImageView(new Image( url));
+    iv.setRotate(rotation);
+    var params = new SnapshotParameters();
+    params.setFill(Color.Transparent);
+    val img = iv.snapshot(params, null);
+    this.fill_=(new ImagePattern(img))
+  }
+
+
+  if(top && left && right && bottom) {
+    this.createImage("4road.png",0)
+  } else if(top && right && left) {
+    this.createImage("3road.png",180)
+  } else if(top && right && bottom) {
+    this.createImage("3road.png",270)
+  } else if(top && left && bottom) {
+    this.createImage("3road.png",90)
+  } else if(left && right && bottom) {
+    this.createImage("3road.png",0)
+  } else if(top && bottom) {
+    this.createImage("2roadLine.png",90)
+  } else if(right && left) {
+    this.createImage("2roadLine.png",0)
+  } else if(top && right) {
+    this.createImage("2road.png",270)
+  } else if(top && left) {
+    this.createImage("2road.png",180)
+  } else if(bottom && right) {
+    this.createImage("2road.png",0)
+  } else if(bottom && left) {
+    this.createImage("2road.png",90)
+  } else if(bottom) {
+    this.createImage("1road.png",90)
+  } else if(top) {
+    this.createImage("1road.png",270)
+  } else if(right) {
+    this.createImage("1road.png",0)
+  } else if(left) {
+    this.createImage("1road.png",180)
+  }
 
   if(!top) {
     var border =  new Rectangle() {
