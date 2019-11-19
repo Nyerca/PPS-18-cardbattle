93ac734ed866a8f03eeb0c20ea64791bbcf53773
lorenzo <lorenzo.bacchiani@studio.unibo.it>
2019-11-19 16:53:54 +0100

    implemented card, player and game

--- src/main/scala/model/Player.scala
+++ src/main/scala/model/Player.scala
@@ -1,38 +1,33 @@
 package model
 
-import javafx.scene.paint.ImagePattern
-import scalafx.scene.image.Image
-import scalafx.scene.paint.Color
-import scalafx.scene.shape.{Circle, Rectangle}
-import scalafx.Includes._
+trait Player {
+  def name: String
+  def level: Int
+  def image: String
+  def battleDeck: List[Card]
+  def healthPoint: Double
+}
 
-class Player (var _position : RectangleCell) {
-  private var _icon:Rectangle = new Rectangle() {
-    x=_position.getX+_position.getWidth/2 -30
-    y=(_position.getY+_position.getHeight/2)-70
-    width = 60
-    height = 80
+class User(override val name: String, override val image: String, var level: Int,  var missingExperience: Double, var battleDeck: List[Card], var allCards: List[Card], var healthPoint: Double) extends Player {
+
+  def addExperience(exp: Int): Unit = missingExperience - exp match {
+    case 0 =>
+      level += 1
+      missingExperience = 5 * level - (exp - missingExperience)
+    case _ => missingExperience -= exp
   }
 
-  def icon = _icon;
-
-
-
-  def image_(url: String) = {
-    var im = new Image( url);
-    _icon.fill_=(new ImagePattern(im))
-  }
-  image_("bot.png")
-
-
-
-
-  def position = _position;
-  def position_ (value:RectangleCell, url: String) :Unit = {
-    _position = value;
-    println("New player position: (" + _position.getX + ", "+ _position.getY+ ")")
-
-
-    image_(url)
+  def gainCard(card: Card): Unit = allCards.find(c => c.name == card.name) match {
+    case Some(c) => c.incrementCardNumber()
+    case _ => allCards = card :: allCards
   }
 }
+
+case class Enemy(override val name: String, override val level: Int, override val image: String, override val battleDeck: List[Card], override val healthPoint: Double) extends Player
+
+object Player {
+  def UserFactory(name: String, image: String, allCards: List[Card] = List(), battleDeck: List[Card] = List(), level: Int = 1, healthPoint: Double = 3, missingExperience: Double = 4): User = new User(name, image, level, missingExperience, battleDeck, allCards, healthPoint)
+  def EnemyFactory(name: String, image: String, battleDeck: List[Card], level: Int = 1, healthPoint: Double = 3): Enemy = Enemy(name, level, image, battleDeck, healthPoint)
+}
+
+
