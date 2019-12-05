5ca4aa0bdf25546b0ab53e55e15eafd9d73305ab
lorenzo <lorenzo.bacchiani@studio.unibo.it>
2019-12-05 13:04:51 +0100

    game over scene graphic improvement

--- src/main/scala/view/scenes/GameOverScene.scala
+++ src/main/scala/view/scenes/GameOverScene.scala
@@ -15,12 +15,13 @@ class GameOverScene(override val parentStage: Stage, val gameController: GameCon
   stylesheets.add("style.css")
 
 
-  val brokenHeart: Button = GUIObjectFactory.buttonFactory(450, 200, mouseTransparency = true)("brokenHeart")
+  val brokenHeart: Button = GUIObjectFactory.buttonFactory(530, 200, mouseTransparency = true)("brokenHeart")
+  val anubiDefeated: Button = GUIObjectFactory.buttonFactory(500, 350, mouseTransparency = true)("anubiDefeated")
   val retryButton: Button = GUIObjectFactory.buttonFactory(500,550, mouseTransparency = false, handle(gameController.setScene(this, MainScene(parentStage))), GUIObjectFactory.DEFAULT_STYLE, "Retry")("retry")
   root = new Pane {
     styleClass.add("common")
     style = "-fx-background-color: black;"
-    children = List(brokenHeart, retryButton)
+    children = List(brokenHeart, retryButton, anubiDefeated)
   }
   TransitionFactory.translateTransitionFactory(Duration(1000), brokenHeart, TransitionFactory.DEFAULT_ON_FINISHED, 0, -100, Transition.Indefinite, autoReversible = true).play()
 }
