516ebf3703f398c0d3c6149879232ac4cf92fdfd
lorenzo <lorenzo.bacchiani@studio.unibo.it>
2019-12-05 22:36:14 +0100

    css improvement

--- src/main/scala/view/scenes/GameOverScene.scala
+++ src/main/scala/view/scenes/GameOverScene.scala
@@ -15,8 +15,8 @@ class GameOverScene(override val parentStage: Stage, val gameController: GameCon
   stylesheets.add("style.css")
 
 
-  val brokenHeart: Button = GUIObjectFactory.buttonFactory(530, 200, mouseTransparency = true)("brokenHeart")
-  val anubiDefeated: Button = GUIObjectFactory.buttonFactory(500, 350, mouseTransparency = true)("anubiDefeated")
+  val brokenHeart: Button = GUIObjectFactory.buttonFactory(550, 200, mouseTransparency = true)("brokenHeart")
+  val anubiDefeated: Button = GUIObjectFactory.buttonFactory(470, 310, mouseTransparency = true)("anubiDefeated")
   val retryButton: Button = GUIObjectFactory.buttonFactory(500,550, mouseTransparency = false, handle(gameController.setScene(this, MainScene(parentStage))), GUIObjectFactory.DEFAULT_STYLE, "Retry")("mainPageButton")
   root = new Pane {
     styleClass.add("common")
